2a9f49681dbae9da2279a0ec670e734617920af9
three-body-physics <mintpie@googlemail.com>
2017-08-30 13:45:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ var sentiment = require("sentiment");
 
 const agent = new twitter({
 
-  consumer_key: 'akeae4a68JRbcUfhTbjiCuDWq',
-  consumer_secret: '8Zh9G76yNMjyBm8A3FU9fNCr6OBgLpIUTEllhvzlmho26msVUO',
-  access_token_key: '888069030-fI8kaFKHPm7N6MVEX4TtYZIIdMJeFUT94r6wFdrW',
-  access_token_secret: 'UXi2mjKlmy5SD2pN9UVOHRYHeYRSmWVuKSgRw4QzpyQJe'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 
 });
 
