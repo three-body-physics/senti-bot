5bead2a6ad2d855fefd2fb1a745a8c7321ab31f3
three-body-physics <mintpie@googlemail.com>
2017-09-18 21:12:35 +0100

    latest

---
+++ key.js
@@ -0,0 +1,10 @@
+const apiKey = {
+
+    consumer_key: 'akeae4a68JRbcUfhTbjiCuDWq',
+    consumer_secret: '8Zh9G76yNMjyBm8A3FU9fNCr6OBgLpIUTEllhvzlmho26msVUO',
+    access_token_key: '888069030-fI8kaFKHPm7N6MVEX4TtYZIIdMJeFUT94r6wFdrW',
+    access_token_secret: 'UXi2mjKlmy5SD2pN9UVOHRYHeYRSmWVuKSgRw4QzpyQJe'
+
+}
+
+module.exports = apiKey;
